60b360b45656398d57c9bd28df786839fd7bcdfa
zyli <Zhiyong.Li@flatironssolutions.com>
2018-04-26 11:45:07 +0800

    function
    function test

---
+++ function.js
@@ -0,0 +1,16 @@
+function say(word) {
+    console.log(word);
+}
+
+function execute (someFunction, value) {
+    someFunction(value);
+}
+
+execute(say, "hello");
+
+
+function execute(someFunction, value) {
+    someFunction(value);
+}
+
+execute(function(word){ console.log(word)}, 'Hello');
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
-http.createServer(function (request, response){
+
+function onRequest(request, response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello World\n');
-}).listen(8888);
+}
+http.createServer(onRequest).listen(8888);
 
 console.log('Server running at http://127.0.0.1:8888');
